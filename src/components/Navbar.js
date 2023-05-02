5885a50fee0ebc64aee41b9fe343961e441bccbc
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-03 01:43:08 +0300

    Add and style navigation bar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,63 +1,23 @@
-import React, {useState} from "react";
-import { NavLink } from "react-router-dom";
-
+import React from "react";
 import Container from 'react-bootstrap/Container';
-
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 
-import Offcanvas from 'react-bootstrap/Offcanvas';
-// import CloseButton from 'react-bootstrap/CloseButton';
-
 const MyNavbar = () => {
-   const [expanded,setExpanded] = useState(false)
-   const handleSelect = () =>  setExpanded(false)
-   
-    // <nav>
-    //     <a href="#"><span>Fikerte Tesfaye</span></a>
-    //     <button><i class="fas fa-bars"></i></button>
-    //     <ul>
-    //         <li>
-    //             <NavLink to="/" activeclassname="active">Portfolio</NavLink>
-    //         </li>
-    //         <li>
-    //             <NavLink to="/about" activeclassname="active">About</NavLink>
-    //         </li>
-    //         <li>
-    //             <NavLink to="/contact" activeclassname="active">Contact</NavLink>
-    //         </li>
-    //     </ul>
-    // </nav>
     return (
-      <Navbar collapseOnSelect bg="light" expand="md" className="mb-3">
-        {console.log({expanded})}
-        <Container fluid>
-          <Navbar.Brand href="#">Navbar Offcanvas</Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar-expand-expand"/>
-        {console.log({expanded})}
-
-          <Navbar.Offcanvas
-            id="offcanvasNavbar-expand-expand"
-            aria-labelledby="offcanvasNavbarLabel-expand-expand"
-            placement="end"
-          >
-            <Offcanvas.Header closeButton>
-              <Offcanvas.Title id="offcanvasNavbarLabel-expand-expand">
-
-              </Offcanvas.Title>
-              {/* <CloseButton variant="white" onClick={()=>({expand} = true)}/> */}
-            </Offcanvas.Header>
-            <Offcanvas.Body>
-              <Nav className="justify-content-end flex-grow-1 pe-3">
-                <button type="button" className="btn btn-primary" ><NavLink to="/">Portfolio</NavLink></button>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-              </Nav>
-            </Offcanvas.Body>
-          </Navbar.Offcanvas>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Navbar.Brand href="#">Fikerte Tesfaye</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+              <Nav className="ms-auto">
+                  <Nav.Link href="#portfolio">Portfolio</Nav.Link>
+                  <Nav.Link href="#about">About</Nav.Link>
+                  <Nav.Link href="#contact">Contact</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
         </Container>
       </Navbar>
-  
     );
     
 };
