7bd0ac2b84fd36fd4aa201a7b03411369cb337b2
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-09 13:30:39 +0300

    Add custom style that overwrites Bootstrap CSS

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,7 +1,10 @@
 import React from "react";
 
 const Portfolio = () => (
-    <div className="p">
+    <div className="portfolio-container">
+        <div className="header-container">
+					
+				</div>
 
     </div>
 )
