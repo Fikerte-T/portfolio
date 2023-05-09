0651b26084a7f078c79736d882497d049221711b
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-09 13:03:25 +0300

    Avoid page reloading when a navigation link is selected

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
@@ -11,9 +12,9 @@ const MyNavbar = () => {
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
             <Navbar.Collapse id="basic-navbar-nav">
               <Nav className="ms-auto">
-                  <Nav.Link href="#portfolio">Portfolio</Nav.Link>
-                  <Nav.Link href="#about">About</Nav.Link>
-                  <Nav.Link href="#contact">Contact</Nav.Link>
+                  <Nav.Link as={Link} to="/" className={isActive=>(isActive ? 'active' : '')}>Portfolio</Nav.Link>
+                  <Nav.Link as={Link} to="about">About</Nav.Link>
+                  <Nav.Link as={Link} to="contact">Contact</Nav.Link>
                 </Nav>
             </Navbar.Collapse>
         </Container>
