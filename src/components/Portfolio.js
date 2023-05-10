50f02a4a7339c45555eaf1dfe4b708a9cdc24bcd
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-10 16:34:20 +0300

    Add portfolio component

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -2,9 +2,20 @@ import React from "react";
 
 const Portfolio = () => (
     <div className="portfolio-container">
-        <div className="header-container">
-					
+      <div className="header-container">
+				<div className="header-text">
+					<h1>I'm Fikerte.</h1>
+					<h1>Glad to see you!</h1>
+					<p>I’m a software developer! I can help you build a product, 
+						feature or website Look through some of my work and experience! 
+						If you like what you see and have a project you need coded, don’t hestiate to contact me.
+					</p>
+					<div className="links">
+						<a href="https://www.linkedin.com/in/fikerte-tesfaye/">Let's connect</a>
+					</div>
 				</div>
+				
+			</div>
 
     </div>
 )
