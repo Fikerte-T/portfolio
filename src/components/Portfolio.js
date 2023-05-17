2804ee74370cdbd0b9f8841a787824e4c9bc2d93
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-17 14:14:42 +0300

    Move images folder to src

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -1,8 +1,10 @@
 import React from "react";
-
+import Button from 'react-bootstrap/Button';
+import Snapshoot_Portfolio_desktop_3 from '../../images/Snapshoot_Portfolio_desktop_3.png';
 const Portfolio = () => (
-    <div className="portfolio-container">
-      <div className="header-container">
+	<>
+		<div className="portfolio-container">
+		<div className="header-container">
 				<div className="header-text">
 					<h1>I'm Fikerte.</h1>
 					<h1>Glad to see you!</h1>
@@ -13,11 +15,24 @@ const Portfolio = () => (
 					<div className="links">
 						<a href="https://www.linkedin.com/in/fikerte-tesfaye/">Let's connect</a>
 					</div>
-				</div>
-				
+				</div>	
 			</div>
-
-    </div>
+		</div>
+		<div className="project-cards">
+		<img src= {Snapshoot_Portfolio_desktop_3} alt="my-project"/>
+		<div className="card-text">
+			<h2>Project name</h2>
+			<p>Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.</p>
+			<ul>
+				<li>HTML</li>
+				<li>CSS</li>
+				<li>Javascript</li>
+				<li>Ruby on Rails</li>
+			</ul>
+			<Button variant="primary">See Project</Button>{' '}
+		</div>
+	</div>
+</>
 )
 
 export default Portfolio;
