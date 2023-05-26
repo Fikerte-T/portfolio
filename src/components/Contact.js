4e9471fd5396e9c480a5fc9a5eb8ccc75defdba9
Fikerte-T <tofikertesfaye@gmail.com>
2023-05-26 23:20:17 +0300

    Add contact me page and style

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,42 +21,43 @@ const Contact = () => {
       setValue(event.target.value);
     };
     return (
-        <div className=" contact-container portfolio-container">
-        <div className="form-container header-container">
-            <div className="form">
-                <h2 className="heading-2">Contact me</h2>
-                <p className="paragraph">If you have an application you are interested in developing,
-                     a feature that you need built or a project that needs coding. 
-                     I’d love to help with it!
-                </p>
-                <Form noValidate validated={validated} onSubmit={handleSubmit}>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Email address"
-                        className="mb-3"
-                    >
-                        <Form.Control type="email" placeholder="name@example.com" required />
-                        <Form.Control.Feedback type="invalid">
-                        Please provide a valid email.
-                        </Form.Control.Feedback>
-                    </FloatingLabel>
-                    <FloatingLabel
-                        controlId="floatingInput"
-                        label="Your message"
-                        className="mb-3"
-                    >
-                     <Form.Control as="textarea" placeholder="Enter your message " required onChange={onChange} style={{height: "200px"}}/>
-                        <Form.Control.Feedback type="invalid">
-                        Please write a message.
-                        </Form.Control.Feedback>
-                    </FloatingLabel>
-                      
+        <div className="contact-container portfolio-container">
            
-                    <Button type="submit">Get in touch</Button>
-                </Form>
-            </div>
+            <div className="form-container header-container">
+                <div className="form">
+                    <h2 className="heading-2">Contact me</h2>
+                    <p className="paragraph">If you have an application you are interested in developing,
+                        a feature that you need built or a project that needs coding. 
+                        I’d love to help with it!
+                    </p>
+                    <Form noValidate validated={validated} onSubmit={handleSubmit}>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Email address"
+                            className="mb-3"
+                        >
+                            <Form.Control type="email" placeholder="name@example.com" required />
+                            <Form.Control.Feedback type="invalid">
+                            Please provide a valid email.
+                            </Form.Control.Feedback>
+                        </FloatingLabel>
+                        <FloatingLabel
+                            controlId="floatingInput"
+                            label="Your message"
+                            className="mb-3"
+                        >
+                        <Form.Control as="textarea" placeholder="Enter your message " required onChange={onChange} style={{height: "200px"}}/>
+                            <Form.Control.Feedback type="invalid">
+                            Please write a message.
+                            </Form.Control.Feedback>
+                        </FloatingLabel>
+                        
+            
+                        <Button type="submit">Get in touch</Button>
+                    </Form>
+                </div>
+            </div>    
         </div>
-    </div>
     )
     
 }
